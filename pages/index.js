7cc6f6adb02fb5c9aef4416bacd7e46f05a1b622
trump-odoo <91389086+trump-odoo@users.noreply.github.com>
2023-12-26 20:58:58 -0300

    Update index.js
    correcao

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Olá Paty este é no nosso site!" />
+        <Header title="Olá Paty este é nosso site!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
